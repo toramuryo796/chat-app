ca703dda1d5d91625a302190aa908b340d36144a
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-07-23 07:45:03 +0900

    チャットルーム作成機能の実装

--- db/migrate/20200721225854_create_room_users.rb
+++ db/migrate/20200721225854_create_room_users.rb
@@ -1,8 +1,8 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.references :user, foreig_key: true
-      t.references :room, foreig_key: true
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -40,4 +40,6 @@ ActiveRecord::Schema.define(version: 2020_07_21_225854) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
